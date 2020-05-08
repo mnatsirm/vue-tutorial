eeffac9ec73a7414ae85129e82160eb963b3628a
mnatsirm <mnatsirm53@gmail.com>
2020-05-08 22:33:09 +0700

    refactor into using shorthand syntax; v-bind and v-on

--- 01-upvote/main.js
+++ 01-upvote/main.js
@@ -2,13 +2,13 @@ const submissionComponent = {
     template:
         `<div style="display: flex; width: 100%"> 
             <figure class="media-left">
-                <img class="image is-64x64" v-bind:src="submission.submissionImage">
+                <img class="image is-64x64" :src="submission.submissionImage">
             </figure>
             <div class="media-content">
                 <div class="content">
                     <p>
                         <strong>
-                            <a v-bind:href="submissions.url" class="has-text-info">{{ submission.title }}</a>
+                            <a :href="submissions.url" class="has-text-info">{{ submission.title }}</a>
                             <span class="tag is-small">#{{ submission.id }}</span>
                         </strong>
                         <br>
@@ -16,13 +16,13 @@ const submissionComponent = {
                         <br>
                         <small class="is-size-7">
                             Submitted by:
-                            <img class="image is-24x24" v-bind:src="submission.avatar">
+                            <img class="image is-24x24" :src="submission.avatar">
                         </small>
                     </p>
                 </div>
             </div>
             <div class="media-right">
-                <span class="icon is-small" v-on:click="upvote(submission.id)">
+                <span class="icon is-small" @click="upvote(submission.id)">
                     <i class="fa fa-chevron-up"></i>
                     <strong class="has-text-info">{{ submission.votes }}</strong>
                 </span>
